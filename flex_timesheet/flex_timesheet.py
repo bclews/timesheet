6f3988ca852756b4e2c1ccbb647b5dba9c347884
Ben Clews <ben.clews@csiro.au>
2022-02-15 14:58:49 +1100

    wip

--- flex_timesheet/flex_timesheet.py
+++ flex_timesheet/flex_timesheet.py
@@ -1,8 +1,7 @@
-from ast import Try
 import parse
-from datetime import datetime
+import json
 
-# FILENAME = 'should-be-defined-in-a-configuration-file.json'
+FILENAME = 'flex_timesheet.json'
 
 # WEEKLY_TIMESHEET_TEMPLATE = {
 #     "week_starting": "",
@@ -21,13 +20,26 @@ from datetime import datetime
 #     "sick": []
 # }
 
+def retrieve_timesheets():
+    with open(FILENAME) as timesheet_file:
+        return json.load(timesheet_file)
+
+def save_timesheets(timesheet):
+    with open(FILENAME, 'w') as timesheet_file:
+        timesheet_json = json.dumps(timesheet, indent=4)
+        timesheet_file.write(timesheet_json)
 
 def work(time_start, time_end, the_date):
     try:
         start = parse.get_datetime(the_date, time_start)
         end = parse.get_datetime(the_date, time_end)
+        work = {"start": start.isoformat(), "end": end.isoformat()}
 
-        print(f"{start.isoformat() =}")
-        print(f"{end.isoformat() =}")
+        timesheets = retrieve_timesheets()
+        for timesheet in timesheets["time_sheets"]:
+            if timesheet["week_starting"] == "2022-02-14":                
+                timesheet["work"].append(work)
+                save_timesheets(timesheets)
+                break
     except parse.InputException as error:
         print(error)
