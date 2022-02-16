32c677406b210d7e59e2993678a541bf1005d3a2
Ben Clews <ben.clews@csiro.au>
2022-02-16 12:14:52 +1100

    wip

--- flex_timesheet/flex_timesheet.py
+++ flex_timesheet/flex_timesheet.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import calculations
 import parse
 import json
 
@@ -31,13 +33,18 @@ def save_timesheets(timesheet):
 
 def work(time_start, time_end, the_date):
     try:
-        start = parse.get_datetime(the_date, time_start)
-        end = parse.get_datetime(the_date, time_end)
-        work = {"start": start.isoformat(), "end": end.isoformat()}
+        work_start = parse.get_datetime(the_date, time_start)
+        work_end = parse.get_datetime(the_date, time_end)
+        work = {"start": work_start.isoformat(), "end": work_end.isoformat()}
+        
+        work_date = parse.get_date(the_date)
+        work_week_starting_date = calculations.find_date_of_previous_monday(work_date)
 
         timesheets = retrieve_timesheets()
-        for timesheet in timesheets["time_sheets"]:
-            if timesheet["week_starting"] == "2022-02-14":                
+        for timesheet in timesheets:
+            timesheet_week_starting_date = parse.get_date(timesheet["week_starting"])
+
+            if timesheet_week_starting_date == work_week_starting_date:
                 timesheet["work"].append(work)
                 save_timesheets(timesheets)
                 break
--- flex_timesheet/parse.py
+++ flex_timesheet/parse.py
@@ -32,3 +32,9 @@ def get_datetime(the_date, the_time):
         raise InputException("The time must be of format HH:MM, e.g. 08:00 or 17:00")
 
     return datetime.strptime(f"{the_date} {the_time}", "%Y-%m-%d %H:%M")
+
+def get_date(the_date):
+    if invalid_date(the_date):
+        raise InputException("The date must be of format YYYY-mm-dd, e.g. 2021-01-12")
+
+    return datetime.strptime(f"{the_date}", "%Y-%m-%d")
