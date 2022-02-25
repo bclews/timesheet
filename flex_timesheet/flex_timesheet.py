228704c10b0a9bbb7dc86d855e0ada38e09742f5
Bennett Clews <bennett.clews@gmail.com>
2022-02-25 22:23:42 +1100

    wip

--- flex_timesheet/flex_timesheet.py
+++ flex_timesheet/flex_timesheet.py
@@ -4,24 +4,7 @@ import calculations
 import parse
 import json
 
-FILENAME = 'flex_timesheet.json'
-
-# WEEKLY_TIMESHEET_TEMPLATE = {
-#     "week_starting": "",
-#     "standard_working_hours_per_week": {
-#         "hours": 36,
-#         "minutes": 45,
-#         "days_in_working_week": 5
-#     },
-#     "flextime_balance": {
-#         "days": 0,
-#         "seconds": 0
-#     },
-#     "work": [],
-#     "flex": [],
-#     "holiday": [],
-#     "sick": []
-# }
+FILENAME = "flex_timesheet.json"
 
 #
 # File handling...
@@ -30,8 +13,9 @@ def retrieve_timesheets():
     with open(FILENAME) as timesheet_file:
         return json.load(timesheet_file)
 
+
 def save_timesheets(timesheet):
-    with open(FILENAME, 'w') as timesheet_file:
+    with open(FILENAME, "w") as timesheet_file:
         timesheet_json = json.dumps(timesheet, indent=4)
         timesheet_file.write(timesheet_json)
 
@@ -39,30 +23,44 @@ def save_timesheets(timesheet):
 #
 # Timesheet handling...
 #
+def create_new_timesheet(the_date, event_type, event_log):
+    new_timesheet = {
+        "week_starting": the_date.strftime("%Y-%m-%d"),
+        "work": [],
+        "flex": [],
+        "holiday": [],
+        "sick": [],
+    }
+    new_timesheet[event_type].append(event_log)
+    return new_timesheet
+
+
 def get_week_start(the_date):
     parsed_date = parse.get_date(the_date)
     return calculations.find_date_of_previous_monday(parsed_date)
+
 
 def create_event_log(time_start, time_end, the_date):
     start = parse.get_datetime(the_date, time_start)
     end = parse.get_datetime(the_date, time_end)
     return {"start": start.isoformat(), "end": end.isoformat()}
 
-def add_event(event_type, time_start, time_end, the_date):
-    # TODO: check event_type in [work, flex, holiday, sick]
-    # TODO: what happens if no timesheet entry for starting date?
 
+def add_event(event_type, time_start, time_end, the_date):
     event_log = create_event_log(time_start, time_end, the_date)
     week_start = get_week_start(the_date)
-    
+
     timesheets = retrieve_timesheets()
     for timesheet in timesheets:
-        timesheet_week_starting_date = parse.get_date(timesheet["week_starting"])
-
-        if timesheet_week_starting_date == week_start: 
+        if parse.get_date(timesheet["week_starting"]) == week_start:
             timesheet[event_type].append(event_log)
             save_timesheets(timesheets)
             break
+    else:
+        timesheet = create_new_timesheet(week_start, event_type, event_log)
+        timesheets.append(timesheet)
+        save_timesheets(timesheets)
+
 
 #
 #
@@ -73,17 +71,20 @@ def work(time_start, time_end, the_date):
     except parse.InputException as error:
         print(error)
 
+
 def flex(time_start, time_end, the_date):
     try:
         add_event("flex", time_start, time_end, the_date)
     except parse.InputException as error:
         print(error)
 
+
 def sick(time_start, time_end, the_date):
     try:
         add_event("sick", time_start, time_end, the_date)
     except parse.InputException as error:
         print(error)
+
 
 def holiday(time_start, time_end, the_date):
     try:
