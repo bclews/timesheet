669fec18d70c9fc629bac599d9ff7da3f58766dc
Ben Clews <ben.clews@csiro.au>
2022-03-02 12:22:30 +1100

    wip

--- flex_timesheet/flex_timesheet.py
+++ flex_timesheet/flex_timesheet.py
@@ -1,6 +1,7 @@
 import calculations
 import parse
 import json
+import typer
 
 FILENAME = "flex_timesheet.json"
 
@@ -57,29 +58,62 @@ def add_event(event_type, time_start, time_end, the_date):
     week_start = get_week_start(the_date)
 
     timesheet_file = retrieve_timesheet_file()
-    for timesheets in timesheet_file["timesheets"]:
+    for timesheet in timesheet_file["timesheets"]:
         # find existing timesheets
-        if parse.get_date(timesheets["week_starting"]) == week_start:
-            timesheets[event_type].append(event_log)
+        if parse.get_date(timesheet["week_starting"]) == week_start:
+            timesheet[event_type].append(event_log)
             save_timesheet_file(timesheet_file)
             break
     else:
         # a timesheets does not yet exist for the given date, so create a new one
-        timesheets = create_new_timesheet(week_start, event_type, event_log)
-        timesheet_file['timesheets'].append(timesheets)
+        timesheet = create_new_timesheet(week_start, event_type, event_log)
+        timesheet_file['timesheets'].append(timesheet)
         save_timesheet_file(timesheet_file)
 
 #
 # Timesheet reporting...
 #
-def report():
-    timesheet_file = retrieve_timesheet_file()
-    for timesheets in timesheet_file["timesheets"]:
-        print(timesheets["week_starting"])
+# Week starting: 28th February 2022
+# Starting flextime balance: 2 hours, 22 minutes
+# Total logged hours: 33 hours, 33 minutes
+#
+# Hours work: 22 hours, 22 minutes
+#        Monday 08:00-12:00 
+#        Monday 13:00-17:00
+#       Tuesday 08:00-12:00 
+#       Tuesday 13:00-17:00
+#     Wednesday 08:00-12:00 
+#     Wednesday 13:00-17:00
+#      Thursday 08:00-12:00 
+#      Thursday 13:00-17:00
+#        Friday 08:00-12:00 
+#        Friday 13:00-17:00
+#
+# Hours flex: 0 hours, 0 minutes
+#
+# Hours holiday: 0 hours, 0 minutes
+#
+# Hours sick: 0 hours, 0 minutes
+def report(the_date):
+    week_start = get_week_start(the_date)
 
-    print()
-    print()
+    report = []
+    report.append( typer.style(f"Week starting: ", bold=True) + week_start.strftime("%Y-%m-%d"))
     
-    newlist = sorted(timesheet_file["timesheets"], key=lambda d: d['week_starting']) 
-    for i in newlist:
-        print(i["week_starting"])
+    timesheet_file = retrieve_timesheet_file()
+    for timesheet in timesheet_file["timesheets"]:
+        # find existing timesheets
+        if parse.get_date(timesheet["week_starting"]) == week_start:
+            hours_work, minutes_work, seconds_work = calculations.aggregate(timesheet["work"])
+            hours_flex, minutes_flex, seconds_flex = calculations.aggregate(timesheet["flex"])
+            hours_holiday, minutes_holiday, seconds_holiday = calculations.aggregate(timesheet["holiday"])
+            hours_sick, minutes_sick, seconds_sick = calculations.aggregate(timesheet["sick"])
+            
+            report.append( typer.style(f"   Hours work: ", bold=True) + f"{hours_work} hours, {minutes_work} minutes")
+            report.append( typer.style(f"   Hours flex: ", bold=True) + f"{hours_flex} hours, {minutes_flex} minutes")
+            report.append( typer.style(f"Hours holiday: ", bold=True) + f"{hours_holiday} hours, {minutes_holiday} minutes")
+            report.append( typer.style(f"   Hours sick: ", bold=True) + f"{hours_sick} hours, {minutes_sick} minutes")
+
+            break
+
+    return report
